5a81413c7f12ebce731984565d4e58807439e378
SandeepanProjects <>
2025-03-09 20:28:17 +0530

    remove uwated code

--- MVVMApiCallSwiftUI/Views/ContentView.swift
+++ MVVMApiCallSwiftUI/Views/ContentView.swift
@@ -35,29 +35,3 @@ struct ContentView: View {
     }
 }
 
-//struct GridView: View {
-//    let items: [Product]
-//    
-//    let columns = [GridItem(.flexible()), GridItem(.flexible())]  // Define the number of columns in the grid
-//    
-//    var body: some View {
-//        ScrollView {
-//            LazyVGrid(columns: columns, spacing: 20) {
-//                ForEach(items, id: \.id) { item in
-//                    VStack {
-//                        Text(String(item.productId))
-//                            .font(.headline)
-//                        Text(String(item.quantity))
-//                            .font(.subheadline)
-//                            .lineLimit(2)
-//                            .truncationMode(.tail)
-//                    }
-//                    .padding()
-//                    .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
-//                }
-//            }
-//            .padding()
-//        }
-//    }
-//}
-
